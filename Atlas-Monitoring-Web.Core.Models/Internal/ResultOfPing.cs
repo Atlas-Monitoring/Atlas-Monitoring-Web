e975146ee226f8d21f09e3daa35ba8b6e514aed8
Paulin <buland2001@gmail.com>
2024-09-29 16:47:37 +0200

    Add ping device feature

---
+++ Atlas-Monitoring-Web.Core.Models/Internal/ResultOfPing.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas_Monitoring_Web.Core.Models.Internal
+{
+    public class ResultOfPing
+    {
+        public long ResponseTimeInMS { get; set; }
+        public bool PingSucces { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
