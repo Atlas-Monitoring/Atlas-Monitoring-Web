9092f95d44be4cf167dfdfe03294204213ad0b95
Paulin <buland2001@gmail.com>
2024-10-14 22:22:03 +0200

    Feature 0031

---
+++ Atlas-Monitoring-Web.Core.Interfaces/Application/IDeviceHistoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Atlas_Monitoring_Web.Core.Models.ViewModels;
+
+namespace Atlas_Monitoring_Web.Core.Interfaces.Application
+{
+    public interface IDeviceHistoryRepository
+    {
+        #region Create
+
+        #endregion
+
+        #region Read
+        Task<List<DeviceHistoryReadViewModel>> GetHistoryOfADevice(Guid deviceId);
+        #endregion
+
+        #region Update
+
+        #endregion
+
+        #region Delete
+
+        #endregion
+    }
+}
--- Atlas-Monitoring-Web.Core.Models/ViewModels/DeviceHistoryViewModel.cs
+++ Atlas-Monitoring-Web.Core.Models/ViewModels/DeviceHistoryViewModel.cs
@@ -1,9 +1,22 @@
-﻿namespace Atlas_Monitoring_Web.Core.Models.ViewModels
+﻿using Atlas_Monitoring_Web.Core.Models.Internal;
+
+namespace Atlas_Monitoring_Web.Core.Models.ViewModels
 {
-    public class DeviceHistoryViewModel
+    public class DeviceHistoryReadViewModel
     {
         public Guid Id { get; set; }
+        public Guid DeviceId { get; set; }
         public DateTime DateAdd { get; set; }
+        public LogLevel LogLevel { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+
+    public class DeviceHistoryWriteViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid DeviceId { get; set; }
+        public DateTime DateAdd { get; set; }
+        public LogLevel LogLevel { get; set; }
         public string Message { get; set; } = string.Empty;
     }
 }
